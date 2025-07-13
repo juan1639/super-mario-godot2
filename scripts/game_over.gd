5fcc9578adc92fb2318ae260b1cebe28c73a6404
unknown <juaneguiabad1639@gmail.com>
2025-07-13 20:59:52 +0200

    Arreglado resetear-scores al rejugar newGame y algun que otro bug

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,4 +1,6 @@
 extends PanelContainer
 
 func _on_button_pressed():
+	GlobalValues.marcadores["world"][0] = 1
+	GlobalValues.marcadores["world"][1] = 1
 	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
